c666948ef52dde29df71fcbfcb1e0fab83d0f00d
grace k <grace.kuperman5472@gmail.com>
2022-11-21 12:19:58 +0000

    prettier, hello world, import json, console checks, huzzah

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import episodes from "./episodes.json"
+import episodes from "./episodes.json";
+
+console.log(`Imported ${episodes.length} episode(s)`);
+console.log(`First episode's name is ${episodes[0].name}`);
 
 function App(): JSX.Element {
-  return (
-    <> Hello World</>
-  )
+  return <> Hello World</>;
 }
 
 export default App;
