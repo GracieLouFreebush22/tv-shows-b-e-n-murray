1bdaf8cb05b44f3c33f402634c64654694a4124c
grace k <grace.kuperman5472@gmail.com>
2022-11-21 12:11:13 +0000

    imported data, hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { greet } from "./utils/greet";
+import episodes from "./episodes.json"
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return (
+    <> Hello World</>
+  )
 }
 
 export default App;
