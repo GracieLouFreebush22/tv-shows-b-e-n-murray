b4a2b0e43a5715bc4cf9afcd61924c4a3fbf70be
Ben Murray <benmurray573@gmail.com>
2022-11-22 16:50:37 +0000

    added formatting to previous commit

--- src/components/OneEpisodeView.test.ts
+++ src/components/OneEpisodeView.test.ts
@@ -1,5 +1,5 @@
-import { removeHTMLTags } from "./OneEpisodeView"
+import { removeHTMLTags } from "./OneEpisodeView";
 
 test("remove HTML <p> and </p> tags", () => {
-expect(removeHTMLTags("<p>Hello</p>")).toBe("Hello")
+  expect(removeHTMLTags("<p>Hello</p>")).toBe("Hello");
 });
--- src/components/OneEpisodeView.tsx
+++ src/components/OneEpisodeView.tsx
@@ -5,7 +5,6 @@ interface OneEpisodeViewProps {
 }
 
 function OneEpisodeView(props: OneEpisodeViewProps): JSX.Element {
-
   return (
     <div>
       <div>
@@ -14,20 +13,19 @@ function OneEpisodeView(props: OneEpisodeViewProps): JSX.Element {
           {GenerateEpisodeCode(props.episode.season, props.episode.number)}
         </h2>
         <div>
-          <img src={props.episode.image.medium} alt=""/>
-          <p>
-            {removeHTMLTags(props.episode.summary)}
-          </p>
+          <img src={props.episode.image.medium} alt="" />
+          <p>{removeHTMLTags(props.episode.summary)}</p>
         </div>
       </div>
     </div>
   );
 }
 export function removeHTMLTags(epSummary: string): string {
-  if (epSummary.includes("<p>" && "</p>")) { //"<p> Hello </p>"
-    return epSummary.slice(3,-4) // (3 from the start, 4 from the end)
+  if (epSummary.includes("<p>" && "</p>")) {
+    //"<p> Hello </p>"
+    return epSummary.slice(3, -4); // (3 from the start, 4 from the end)
   }
-  return epSummary
+  return epSummary;
 }
 
 export function GenerateEpisodeCode(
